3bb36342a8d5d2f7714a4222da32c88bd48e3db3
azzamsa <me@azzamsa.com>
2021-11-13 12:00:39 +0700

    ci: migrate to ubuntu
    To have the access for `deadsnakes/ppa`.

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,13 @@
 import nox
 
 
-@nox.session(python=["3.9"])
+@nox.session(python=["3.8", "3.9"])
 def lint(session):
     session.install("pylint==2.11.1")
     session.run("pylint", "src")
 
 
-@nox.session(python=["3.9"])
+@nox.session(python=["3.8", "3.9"])
 def tests(session):
     session.install("pytest==6.2.5")
     session.run("pytest", "src")
