a2f5b9db366b69aa30ac4d2948eedbade6177414
azzamsa <me@azzamsa.com>
2021-11-13 15:17:05 +0700

    feat(treehouse): binary search

--- src/treehouse/search.py
+++ src/treehouse/search.py
@@ -4,3 +4,25 @@ def linear_search(numbers, target):
         if value == target:
             return index
     return None
+
+
+def binary_search(numbers, target):
+    """Search a target from the input using binary search algorithm"""
+    first_index = 0
+    last_index = len(numbers) - 1
+
+    while first_index <= last_index:
+        mid_index = (last_index + first_index) // 2
+
+        # best case
+        if numbers[mid_index] == target:
+            return mid_index
+
+        if numbers[mid_index] < target:
+            # discard all numbers BEFORE mid_index
+            first_index = mid_index + 1
+        else:
+            # discard all numbers AFTER mid_index
+            last_index = mid_index - 1
+
+    return None
--- src/treehouse/search_test.py
+++ src/treehouse/search_test.py
@@ -1,16 +1,21 @@
-from treehouse.search import linear_search
+from treehouse.search import linear_search, binary_search
+
+# `range()` is exclusive!
+numbers1 = list(range(1, 11))
+numbers2 = list(range(1, 11))
+numbers3 = list(range(1, 11))
+numbers4 = list(range(1, 101))
 
 
 def test_linear_search():
-    # `range()` is exclusive!
-    numbers = list(range(1, 11))
-    assert linear_search(numbers, 11) is None
+    assert linear_search(numbers1, 11) is None
+    assert linear_search(numbers2, 3) == 2
+    assert linear_search(numbers3, 6) == 5
+    assert linear_search(numbers4, 50) == 49
 
-    numbers = list(range(1, 11))
-    assert linear_search(numbers, 3) == 2
 
-    numbers = list(range(1, 11))
-    assert linear_search(numbers, 6) == 5
-
-    numbers = list(range(1, 101))
-    assert linear_search(numbers, 50) == 49
+def test_binary_search():
+    assert binary_search(numbers1, 11) is None
+    assert binary_search(numbers2, 3) == 2
+    assert binary_search(numbers3, 6) == 5
+    assert binary_search(numbers4, 50) == 49
