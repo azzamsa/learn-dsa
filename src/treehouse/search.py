a13c8401c6e1f31c948767083bd52f8645e0ab56
azzamsa <me@azzamsa.com>
2021-11-13 14:45:02 +0700

    refactor(treehouse): remove unnecessary lines

--- src/treehouse/search.py
+++ src/treehouse/search.py
@@ -1,9 +1,5 @@
-"""Search a target from the input using linear search algorithm"""
-
-
 def linear_search(numbers, target):
     """Search a target from the input using linear search algorithm"""
-    # for index in range(len(input_)):
     for index, value in enumerate(numbers):
         if value == target:
             return index
