e2510400b5877914a90de7568d452d4e083ff942
azzamsa <me@azzamsa.com>
2021-11-13 13:18:50 +0700

    test: 100% coverage

--- src/treehouse/search_test.py
+++ src/treehouse/search_test.py
@@ -2,11 +2,14 @@ from treehouse.search import linear_search
 
 
 def test_linear_search():
-    input_ = list(range(100))
+    input_ = list(range(10))
+    assert linear_search(input_, 11) is None
+
+    input_ = list(range(10))
+    assert linear_search(input_, 3) == 3
+
+    input_ = list(range(10))
     assert linear_search(input_, 5) == 5
 
     input_ = list(range(100))
     assert linear_search(input_, 50) == 50
-
-    input_ = list(range(100))
-    assert linear_search(input_, 3) == 3
