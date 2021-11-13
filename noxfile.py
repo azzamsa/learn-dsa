11da364b059ed421c8643f0ba58247b677ba99ab
azzamsa <me@azzamsa.com>
2021-11-13 12:49:16 +0700

    test: add coverage

--- noxfile.py
+++ noxfile.py
@@ -11,3 +11,11 @@ def lint(session):
 def tests(session):
     session.install("pytest==6.2.5")
     session.run("pytest", "src")
+
+
+@nox.session(python=["3.9"])
+def coverage(session):
+    session.install("coverage==6.1.2")
+    session.install("pytest==6.2.5")
+    session.run("coverage", "run", "-m", "pytest", "src")
+    session.run("coverage", "report", "-m")
