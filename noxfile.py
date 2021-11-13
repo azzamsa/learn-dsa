f43be0527cd1c4b4503d578975414a2012bc887a
azzamsa <me@azzamsa.com>
2021-11-13 12:58:29 +0700

    refactor: move dependencies to a file

--- noxfile.py
+++ noxfile.py
@@ -3,19 +3,18 @@ import nox
 
 @nox.session(python=["3.8", "3.9"])
 def lint(session):
-    session.install("pylint==2.11.1")
+    session.install("-r", "requirements.txt")
     session.run("pylint", "src")
 
 
 @nox.session(python=["3.8", "3.9"])
 def tests(session):
-    session.install("pytest==6.2.5")
+    session.install("-r", "requirements.txt")
     session.run("pytest", "src")
 
 
 @nox.session(python=["3.9"])
 def coverage(session):
-    session.install("coverage==6.1.2")
-    session.install("pytest==6.2.5")
+    session.install("-r", "requirements.txt")
     session.run("coverage", "run", "-m", "pytest", "src")
     session.run("coverage", "report", "-m")
