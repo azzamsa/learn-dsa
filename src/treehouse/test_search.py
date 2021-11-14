ce7528d740ca5c4e244f317e5c20b5e63b7bd1ad
azzamsa <me@azzamsa.com>
2021-11-14 10:18:11 +0700

    docs: add illustration for non-existing target

--- src/treehouse/test_search.py
+++ src/treehouse/test_search.py
@@ -1,4 +1,4 @@
-from treehouse.search import binary_search, linear_search
+from treehouse.search import binary_search, binary_search_recursion, linear_search
 
 numbers = [0, 5, 7, 10, 15]
 
@@ -15,3 +15,12 @@ def test_binary_search():
     assert binary_search(numbers, 15) == 4
     assert binary_search(numbers, 5) == 1
     assert binary_search(numbers, 6) is None
+
+
+def test_binary_search_recursion():
+    assert binary_search_recursion(numbers, 0, 0, 4) == 0
+    assert binary_search_recursion(numbers, 15, 0, 4) == 4
+    assert binary_search_recursion(numbers, 5, 0, 4) == 1
+    assert binary_search_recursion(numbers, 11, 0, 4) is None
+    assert binary_search_recursion(numbers, 4, 0, 4) is None
+    assert binary_search_recursion(numbers, 8, 0, 4) is None
